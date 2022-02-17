df0792b7c092059a45d8c4fb8fbadcdc153503ad
Sudarshan Gajdhane <Sudarshan.Gajdhane@programmers.io>
2022-02-17 22:43:21 +0530

    validation-changes

--- home.js
+++ home.js
@@ -1,14 +1,20 @@
 function formSubmit() {
     let invalidUserName = document.forms["loginForm"]["username"].value;
     let invalidPassword = document.forms["loginForm"]["password"].value;
-    if (invalidUserName == "" || invalidUserName.length < 3 || invalidUserName.length > 10 ||
-        invalidPassword == "" || invalidPassword.length < 3 || invalidPassword.length > 10) {
-        alert("Invalid input or Name must not empty");
-        return false;
+    if (invalidUserName == "" || invalidPassword == "") {
+        alert("Username or Password must not empty");
+        return;
     }
-    var username = document.forms["loginForm"].elements["username"].value;
-    var password = document.forms["loginForm"].elements["password"].value;
-    localStorage.setItem("name", username);
-    localStorage.setItem("pass", password);
-    window.location.href = "./login.html";
+    else if (invalidUserName.length < 3 || invalidUserName.length > 10 || invalidPassword.length < 3 ||
+        invalidPassword.length > 10) {
+        alert("Invalid Input");
+        return;
     }
+    else {
+        var username = document.forms["loginForm"].elements["username"].value;
+        var password = document.forms["loginForm"].elements["password"].value;
+        localStorage.setItem("name", username);
+        localStorage.setItem("pass", password);
+        window.location.href = "./login.html";
+    }
+}
